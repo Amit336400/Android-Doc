b5ab870dad2b6901ddbcdd342d72f5771b17cb00
Amit <amitkundu113920@gmail.com>
2024-10-18 22:36:49 +0530

    Amit

--- app/src/main/java/com/example/androiddoc/Kotlin/C_Loop/Z_Question_for.kt
+++ app/src/main/java/com/example/androiddoc/Kotlin/C_Loop/Z_Question_for.kt
@@ -2,13 +2,18 @@ package com.example.androiddoc.Kotlin.C_Loop
 
 /*
     TODO create a list or array and print it using for loop
+
  */
 
+fun main() {
+
+}
 
 
 /*
 TODO create a list of number and print it using foeEach loop
  */
+
 
 
 /*
